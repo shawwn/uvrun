feaac6219c9a6aede2e7eb618f3a24ea9615046e
Shawn Presser <shawnpresser@gmail.com>
2017-04-03 23:09:58 -0500

    Define sleep and yield

--- uvrun.cc
+++ uvrun.cc
@@ -21,13 +21,37 @@ NAN_METHOD(RunOnce) {
   info.GetReturnValue().Set(r);
 }
 
-NAN_METHOD(RunNoWait) {
+int runNoWait() {
 #ifdef OLD_UV_RUN_SIGNATURE
   int r = uv_run_nowait(uv_default_loop());
 #else
   int r = uv_run(uv_default_loop(), UV_RUN_NOWAIT);
 #endif
+  return r;
+}
+
+NAN_METHOD(RunNoWait) {
+  int r = runNoWait();
   info.GetReturnValue().Set(r);
+}
+
+#ifdef _WINDOWS
+#include <windows.h>
+#else
+#include <unistd.h>
+#define Sleep(x) usleep((x)*1000)
+#endif
+NAN_METHOD(DoSleep) {
+  unsigned int ms = info[0]->Uint32Value();
+  Sleep(ms);
+}
+
+NAN_METHOD(yield) {
+  unsigned int ms = info[0]->Uint32Value();
+  if (ms > 0) {
+    Sleep(ms);
+  }
+  runNoWait();
 }
 
 NAN_MODULE_INIT(init) {
@@ -38,6 +62,10 @@ NAN_MODULE_INIT(init) {
         Nan::GetFunction(New<FunctionTemplate>(RunOnce)).ToLocalChecked());
     Nan::Set(target, New<String>("runNoWait").ToLocalChecked(),
         Nan::GetFunction(New<FunctionTemplate>(RunNoWait)).ToLocalChecked());
+    Nan::Set(target, New<String>("sleep").ToLocalChecked(),
+        Nan::GetFunction(New<FunctionTemplate>(DoSleep)).ToLocalChecked());
+    Nan::Set(target, New<String>("yield").ToLocalChecked(),
+        Nan::GetFunction(New<FunctionTemplate>(yield)).ToLocalChecked());
 }
 
 NODE_MODULE(uvrun, init);
